8ce22d211fcf8b9144eb7aa41e729e0a4e1b00f7
Brandon Chase <brandonchase93@gmail.com>
2020-03-26 19:01:56 -0600

    gotta fix some stuff

--- jokes.js
+++ jokes.js
@@ -1,8 +1,24 @@
 // jokes.js
 
+// TODO Event propegation to get joke buttons working because they are created dynamically
+
+function displayNumJokes() {
+    $('#getJokeNum').submit(function(e) {
+        // alert("form submitted")
+        // alert($('#numJokes').val())
+        $('#displayArea').empty()
+        for (i=0; i<$('#numJokes').val();i++) {
+            $('#displayArea').append(`<button type="button" id="button${i}" class="jokeButtons">Generate Joke</button><div id="jokeDisplay"></div>`)
+        }
+        e.preventDefault()
+    })
+}
+
 // Send request to API and get response as plaintext
 function generateJoke() {
-    $('#jokeButton').click(function() {
+    $('.jokeButtons').on('click', function(e) {
+        alert("pressed")
+
         $.ajax({
             headers: {
                 Accept : 'text/plain',
@@ -11,17 +27,19 @@ function generateJoke() {
             url: 'https://icanhazdadjoke.com/',
             data: 'data',   //TODO try getting rid of this line
             success: function(response) {
-                $('#jokeDisplay').replaceWith(`<div id="jokeDisplay">${response}</div>`)
+                // $('#jokeDisplay').replaceWith(`<div id="jokeDisplay">${response}</div>`)
             },
             error: function() {
                 alert('Failed to get joke')
             }
         })
+        e.preventDefault()
 
     })
 }
 
 $(function() {
     // alert('document ready')
+    displayNumJokes()
     generateJoke()
 })
