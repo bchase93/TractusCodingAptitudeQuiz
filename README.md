f9db365fed9742df022d326a0cd0beb340db5e7d
Brandon Chase <brandonchase93@gmail.com>
2020-03-27 20:09:33 -0600

    Ready

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Code Aptitude Quiz
 
 My solution to the Tractus Coding Aptitude Quiz  
-[Link to spec](https://docs.google.com/document/d/16ZwJ8jd52mi1X2l9aP_XbuEKJVijOtqKmLSJdU97hJs/edit)
 
-## Technologies Used
+## Libraries Used
 
 - jQuery - AJAX and DOM manipulation
 - Bootstrap - styling
--- jokes.js
+++ jokes.js
@@ -7,8 +7,19 @@ function displayNumJokes() {
         // alert("form submitted")
         // alert($('#numJokes').val())
         $('#displayArea').empty()
-        for (i=0; i<$('#numJokes').val();i++) {
-            $('#displayArea').append(`<button type="button" id="button${i}" class="jokeButtons">Generate Joke</button><div id="jokeDisplay"></div>`)
+        numJokes = $('#numJokes').val()
+        if (numJokes > 10) {
+            numJokes = 10
+        }
+        for (i=0; i<numJokes;i++) {
+            $('#displayArea').append(
+                    `<div class="card col-md-4 m-4 bg-primary" class="d-flex justify-content-center">
+                        <textarea style="width:100%" id="jokeDisplay${i}"></textarea>
+                        <br>
+                        <div class="d-flex justify-content-center">
+                            <button type="button" id="button${i}" class="jokeButtons bg-warning">Generate Joke</button>
+                        </div>
+                    </div>`)
         }
         e.preventDefault()
     })
@@ -16,8 +27,7 @@ function displayNumJokes() {
 
 // Send request to API and get response as plaintext
 function generateJoke() {
-    $('.jokeButtons').on('click', function(e) {
-        alert("pressed")
+    $(document).on('click', '.jokeButtons', function(e) {
 
         $.ajax({
             headers: {
@@ -27,7 +37,8 @@ function generateJoke() {
             url: 'https://icanhazdadjoke.com/',
             data: 'data',   //TODO try getting rid of this line
             success: function(response) {
-                // $('#jokeDisplay').replaceWith(`<div id="jokeDisplay">${response}</div>`)
+                let num = e.target.id[e.target.id.length - 1]
+                $(`#jokeDisplay${num}`).replaceWith(`<textarea style="width:100%" id="jokeDisplay${num}">${response}</textarea>`)
             },
             error: function() {
                 alert('Failed to get joke')
